1b32e9bd63f9726c8150fb7a7fb31353e7f707e4
Lucas Resende <lucasrescarneiro@gmail.com>
2019-09-28 20:10:22 -0300

    Added the first laboratory

---
+++ 1-vetor/caVetor2D/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace caVetor2D
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Vetor2D v1 = new Vetor2D();
+			Vetor2D v2 = new Vetor2D();
+			v1.constroeVetor();
+			v2.constroeVetor();
+			Console.WriteLine("Vetor 1 = (" + v1.getX() + " " + v1.getY() + ")");
+			Console.WriteLine("Vetor 2 = (" + v2.getX() + " " + v2.getY() + ")");
+			v1.set(1, 2);
+			v2.set(4, 3);
+			Console.WriteLine("Novo vetor 1 = (" + v1.getX() + " " + v1.getY() + ")");
+			Console.WriteLine("Novo vetor 2 = (" + v2.getX() + " " + v2.getY() + ")");
+			Console.WriteLine("Escalar = " + v1.escalar(v2));
+			Console.WriteLine("Modulo = " + Math.Round(v1.modulo(), 2));
+			Console.WriteLine("Angulo = " + Math.Round(v1.angulo(v2), 2) + "°");
+			v1.projecao(v2);
+		}
+	}
+}
